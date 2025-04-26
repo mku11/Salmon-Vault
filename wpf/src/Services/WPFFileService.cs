6fd0f9ec197d144ed992bbce2131626290fa008e
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-26 10:06:45 +0300

    wpf removed unused param for file service

--- wpf/src/Services/WPFFileService.cs
+++ wpf/src/Services/WPFFileService.cs
@@ -28,7 +28,7 @@ namespace Salmon.Vault.Services;
 
 public class WPFFileService : IFileService
 {
-    public IFile GetFile(string filepath, bool isDirectory)
+    public IFile GetFile(string filepath)
     {
         return new File(filepath);
     }
