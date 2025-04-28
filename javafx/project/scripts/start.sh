05b9714bb515c6550c353b9751987f4be47a7a5c
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-28 12:07:42 +0300

    javafx include javafx in package

--- javafx/project/scripts/start.sh
+++ javafx/project/scripts/start.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-# uncomment and enter the javafx library path
-# export JAVAFX_HOME="/path/to/javafx-sdk-17.0.10"
 
-export SALMON_PATH="./*"
-export JAVAFX_MODULES="--add-modules=javafx.controls,javafx.swing,javafx.fxml,javafx.media,javafx.graphics"
+export SALMON_CLASS_PATH="./*;./libs/*;./salmon/*"
+export JAVAFX_MODULES=--add-modules=javafx.controls,javafx.swing,javafx.fxml,javafx.media,javafx.graphics
+export JAVAFX_DIR=./javafx
+export MAIN_CLASS=com.mku.salmon.vault.main.Main
+java -Djava.library.path="." --module-path $JAVAFX_DIR/lib $JAVAFX_MODULES -cp $SALMON_CLASS_PATH $MAIN_CLASS
 
-if [ -d "$JAVAFX_HOME" ]; then
-	java -Djava.library.path="." --module-path "$JAVAFX_HOME"/lib $JAVAFX_MODULES -cp "$SALMON_PATH" com.mku.salmon.vault.main.Main
-else
-	echo "Could not find JavaFx. Edit this file and set JAVAFX_HOME to the directory where you installed JavaFx 17"
-	echo "Press any key to exit"
-	read -n1
-	exit 1
-fi
