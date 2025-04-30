fbab0e18ad54861a7c675b2354aa0e83f86a9913
mku11 <93253421+mku11@users.noreply.github.com>
2025-04-30 20:25:34 +0300

    android add search for text editor

--- android/src/com/mku/salmon/vault/main/TextEditorActivity.java
+++ android/src/com/mku/salmon/vault/main/TextEditorActivity.java
@@ -23,16 +23,22 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.MenuCompat;
 
@@ -54,6 +60,10 @@ public class TextEditorActivity extends AppCompatActivity {
     private EditText editText;
 
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
+    private SearchView searchView;
+    private String searchString = "";
+
+    private int searchIndex = -1;
 
     public static void setTextFile(AesFile file) {
         TextEditorActivity.file = file;
@@ -66,7 +76,42 @@ public class TextEditorActivity extends AppCompatActivity {
         setContentView(R.layout.texteditor);
         setupControls();
         setupEditor();
+        setupSearchView();
         load();
+    }
+
+    private void setupSearchView() {
+        searchView = new SearchView(getSupportActionBar().getThemedContext());
+        searchView.setFocusable(false);
+        getSupportActionBar().setCustomView(searchView);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        searchView.clearFocus();
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        if (null != searchView) {
+            searchView.setQueryHint("Search");
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setHorizontalGravity(Gravity.RIGHT);
+        }
+        setupSearchViewListeners();
+    }
+
+    public void setupSearchViewListeners() {
+        SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+            public boolean onQueryTextChange(String newText) {
+                if (searchString.equals(newText))
+                    return true;
+                searchString = newText;
+                return true;
+            }
+
+            public boolean onQueryTextSubmit(String query) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(searchView.getWindowToken(), 0);
+                onSearch();
+                return true;
+            }
+        };
+        searchView.setOnQueryTextListener(queryTextListener);
     }
 
     private void setupEditor() {
@@ -95,6 +140,9 @@ public class TextEditorActivity extends AppCompatActivity {
         ((MenuBuilder) menu).setOptionalIconsVisible(true);
         menu.clear();
 
+        menu.add(1, ActionType.SEARCH.ordinal(), 0, getResources().getString(R.string.Search))
+                .setIcon(R.drawable.search_small)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
         menu.add(1, ActionType.SAVE.ordinal(), 0, getResources().getString(R.string.Save))
                 .setIcon(R.drawable.save_small)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
@@ -109,6 +157,9 @@ public class TextEditorActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (ActionType.values()[item.getItemId()]) {
+            case SEARCH:
+                onSearch();
+                break;
             case SAVE:
                 onSave();
                 break;
@@ -121,6 +172,32 @@ public class TextEditorActivity extends AppCompatActivity {
         }
         super.onOptionsItemSelected(item);
         return false;
+    }
+
+    private void onSearch() {
+        if (searchString.length() == 0) {
+            runOnUiThread(() -> {
+                searchView.setIconified(false);
+            });
+            return;
+        }
+        String contents = editText.getText().toString();
+        searchIndex = contents.indexOf(searchString, searchIndex + 1);
+        if (searchIndex >= 0)
+            select(searchIndex, searchString.length());
+    }
+
+    private void select(int start, int length) {
+        runOnUiThread(() -> {
+            Layout layout = editText.getLayout();
+            if (layout != null) {
+                int line = layout.getLineForOffset(start);
+                int y = layout.getLineTop(line);
+                editText.scrollTo(0, y);
+                editText.requestFocus();
+                editText.setSelection(start, start + length);
+            }
+        });
     }
 
     private void load() {
