7ca27ed0b109a0b511817cc420fb53e1f5dcf322
mku11 <93253421+mku11@users.noreply.github.com>
2025-05-04 12:31:00 +0300

    javafx no output for scripts

--- javafx/project/scripts/start.bat
+++ javafx/project/scripts/start.bat
@@ -4,4 +4,4 @@ set SALMON_CLASS_PATH="./*;./libs/*;./salmon/*"
 set JAVAFX_MODULES=--add-modules=javafx.controls,javafx.swing,javafx.fxml,javafx.media,javafx.graphics,javafx.web
 set JAVAFX_DIR=.\javafx
 set MAIN_CLASS=com.mku.salmon.vault.main.Main
-java -Djava.library.path="." --module-path %JAVAFX_DIR%\lib %JAVAFX_MODULES% -cp %SALMON_CLASS_PATH% %MAIN_CLASS%
+java -Djava.library.path="." --module-path %JAVAFX_DIR%\lib %JAVAFX_MODULES% -cp %SALMON_CLASS_PATH% %MAIN_CLASS% >nul 2>&1
--- javafx/project/scripts/start.sh
+++ javafx/project/scripts/start.sh
@@ -4,5 +4,5 @@ export SALMON_CLASS_PATH="./*:./libs/*:./salmon/*"
 export JAVAFX_MODULES=--add-modules=javafx.controls,javafx.swing,javafx.fxml,javafx.media,javafx.graphics,javafx.web
 export JAVAFX_DIR=./javafx
 export MAIN_CLASS=com.mku.salmon.vault.main.Main
-java -Djava.library.path="." --module-path $JAVAFX_DIR/lib $JAVAFX_MODULES -cp $SALMON_CLASS_PATH $MAIN_CLASS
+java -Djava.library.path="." --module-path $JAVAFX_DIR/lib $JAVAFX_MODULES -cp $SALMON_CLASS_PATH $MAIN_CLASS > /dev/null 2>&1
 
