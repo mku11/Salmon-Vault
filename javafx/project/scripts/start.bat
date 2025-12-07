d55baf3d9820070b12350208fc51305e2c71b478
mku11 <93253421+mku11@users.noreply.github.com>
2025-12-07 21:40:08 +0200

    javafx: too many error logging from ice pdf

--- javafx/project/scripts/start.bat
+++ javafx/project/scripts/start.bat
@@ -1,7 +1,7 @@
 @echo on
 
-set SALMON_CLASS_PATH="./*;./libs/*;./salmon/*"
+set SALMON_CLASS_PATH="./*;./libs/*;salmon/*"
 set JAVAFX_MODULES=--add-modules=javafx.controls,javafx.swing,javafx.fxml,javafx.media,javafx.graphics,javafx.web
 set JAVAFX_DIR=javafx
 set MAIN_CLASS=com.mku.salmon.vault.main.Main
-java -Djava.library.path="./salmon" --module-path %JAVAFX_DIR% %JAVAFX_MODULES% -cp %SALMON_CLASS_PATH% %MAIN_CLASS%
+java -Djava.library.path="./salmon" --module-path %JAVAFX_DIR% %JAVAFX_MODULES% -cp %SALMON_CLASS_PATH% %MAIN_CLASS% >nul 2>&1
