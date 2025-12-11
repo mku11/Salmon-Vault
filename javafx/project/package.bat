6b850dd9d0dfafc8380ee26af50b6e67c793761a
mku11 <93253421+mku11@users.noreply.github.com>
2025-12-11 19:45:45 +0200

    javafx: added package script for windows

---
+++ javafx/project/package.bat
@@ -0,0 +1,37 @@
+@echo ON
+set VERSION=3.0.1
+
+:: Salmon Vault javascript
+set OUTPUT_ROOT=..\..\output
+set OUTPUT_DIR=%OUTPUT_ROOT%\javafx
+set SALMON_VAULT_JAR=.\build\libs\salmon-vault-%VERSION%.jar
+set SALMON_LIBS_DIR=.\libs\salmon
+set JAVAFX_LIBS_DIR=.\libs\javafx
+set DEPS_LIBS_DIR=.\libs\libs
+set SALMON_NATIVE_LIBS_DIR=.\build\libs\natives\lib
+set SALMON_SCRIPTS_DIR=.\scripts
+
+set PACKAGES_DIR=app_package
+set SALMON_VAULT=salmon-vault-javafx
+set SALMON_VAULT_PACKAGE_NAME=%SALMON_VAULT%.%VERSION%
+
+powershell mkdir -ErrorAction SilentlyContinue %PACKAGES_DIR%
+del /S /Q .\%PACKAGES_DIR%\*
+
+powershell mkdir -ErrorAction SilentlyContinue %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%
+del /S /Q .\%PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%\*
+
+robocopy /E %SALMON_LIBS_DIR%\ %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%\salmon\
+copy %SALMON_VAULT_JAR% %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%\salmon\
+robocopy /E %JAVAFX_LIBS_DIR%\ %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%\javafx\
+robocopy /E %DEPS_LIBS_DIR%\ %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%\libs\
+copy %SALMON_NATIVE_LIBS_DIR%\*.dll %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%\salmon\
+copy %SALMON_SCRIPTS_DIR%\*.bat %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%\
+copy README.txt %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%\
+
+cd %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%
+powershell -command Compress-Archive -Force -DestinationPath ..\%SALMON_VAULT_PACKAGE_NAME%.zip *
+cd ..\..\
+powershell mkdir -ErrorAction SilentlyContinue %OUTPUT_DIR%
+copy /Y %PACKAGES_DIR%\%SALMON_VAULT_PACKAGE_NAME%.zip %OUTPUT_DIR%
+
