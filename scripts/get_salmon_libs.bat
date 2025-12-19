747f29c7da360cfd70fe9ae2d57cef23c5abeb96
mku11 <93253421+mku11@users.noreply.github.com>
2025-12-19 12:14:59 +0200

    updated salmon lib version 3.0.5

--- scripts/get_salmon_libs.bat
+++ scripts/get_salmon_libs.bat
@@ -2,7 +2,7 @@
 set CURRDIR=%CD%
 
 set DEPS_DIR=..\libs\
-set SALMON_LIB_VERSION=3.0.4
+set SALMON_LIB_VERSION=3.0.5
 :: if you use a snapshot append -SNAPSHOT to this variable
 set SALMON_LIB_BINARY_VERSION=%SALMON_LIB_VERSION%
 
